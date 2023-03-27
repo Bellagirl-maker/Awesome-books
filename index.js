94525862b1cd188763700538905c8254553cc40a
Bellagirl-maker <isabellaotoo25@gmail.com>
2023-03-27 21:16:57 +0000

    Updated Js and readme files

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
-
 const addBtn = document.querySelector('#btn');
-const bookList = document.querySelector('.books-container');
 const titleInput = document.querySelector('#title');
 const authorInput = document.querySelector('#author');
-const removeBtn = document.querySelectorAll('.remove-btn')
 
-const books = JSON.parse(localStorage.getItem('books')) || []
+const books = JSON.parse(localStorage.getItem('books')) || [];
 
-addBtn.addEventListener('click', function () {
-    let size;
-    if (books.length == null) {
-        size = 0
-    } else {
-        size = books.length
-    }
+addBtn.addEventListener('click', () => {
+  let size;
+  if (books.length == null) {
+    size = 0;
+  } else {
+    size = books.length;
+  }
 
-    books.push({ id: size, title: titleInput.value, author: authorInput.value })
-    localStorage.setItem('books', JSON.stringify(books))
-    location.reload()
-})
+  books.push({ id: size, title: titleInput.value, author: authorInput.value });
+  localStorage.setItem('books', JSON.stringify(books));
+  location.reload();
+});
 
 function removeBook(id) {
-    const filtered = books.filter(book => book.id !== id);
-    localStorage.setItem('books', JSON.stringify(filtered))
-    location.reload()
+  const filtered = books.filter((book) => book.id !== id);
+  localStorage.setItem('books', JSON.stringify(filtered));
+  location.reload();
 }
 
 function showBooks() {
-    const booksContainer = document.querySelector('.books-container');
-    for (let i = 0; i < books.length; i++) {
-        booksContainer.innerHTML += `<div><p>
+  const booksContainer = document.querySelector('.books-container');
+  for (let i = 0; i < books.length; i++) {
+    booksContainer.innerHTML += `<div><p>
         ${books[i].title} <br />
         ${books[i].author} 
       </p>
+      
       <button onclick="removeBook(${books[i].id})">Remove</button>
       <hr />
-    </div>`
-    }
+    </div>`;
+  }
 }
 
-window.addEventListener('load', (e) => {
-    showBooks();
-})
+window.addEventListener('load', () => {
+  showBooks();
+});

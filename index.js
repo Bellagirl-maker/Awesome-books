6f8dc95f8ca2453f7c4c6cc73df3cfaf2dd685bc
Bellagirl-maker <isabellaotoo25@gmail.com>
2023-03-28 14:46:55 +0000

    update index.js and style.css files

--- index.js
+++ index.js
@@ -2,21 +2,19 @@ const addBtn = document.querySelector('#btn');
 const titleInput = document.querySelector('#title');
 const authorInput = document.querySelector('#author');
 
-
-
-class book {
+class Book {
   constructor() {
     this.books = JSON.parse(localStorage.getItem('books')) || [];
   }
 
   addBooks(title, author, id) {
-    let newBook = { id, title, author }
+    const newBook = { id, title, author };
     this.books.push(newBook);
-    localStorage.setItem('books', JSON.stringify(this.books))
+    localStorage.setItem('books', JSON.stringify(this.books));
   }
 
   deleteBook(id) {
-    const books = this.books
+    const { books } = this;
     const filtered = books.filter((book) => book.id !== id);
     localStorage.setItem('books', JSON.stringify(filtered));
   }
@@ -26,3 +24,44 @@ class book {
   }
 }
 
+const theBook = new Book();
+
+addBtn.addEventListener('click', () => {
+  let size;
+  if (theBook.books.length == null) {
+    size = 0;
+  } else {
+    size = theBook.books.length;
+  }
+  theBook.addBooks(titleInput.value, authorInput.value, size);
+  location.reload();
+});
+
+function removeBook(id) {
+  theBook.deleteBook(id);
+  location.reload();
+}
+
+function showBooks() {
+  const booksContainer = document.querySelector('.books-container');
+  const bookData = theBook.displayBooks();
+  let cardId = 'odd';
+  for (let i = 0; i < bookData.length; i++) {
+    if (i % 2 === 0) {
+      cardId = 'even';
+    } else {
+      cardId = 'odd';
+    }
+    booksContainer.innerHTML += `<div class="bookcard" id= "${cardId}"><p>
+        "${bookData[i].title}" by 
+        ${bookData[i].author} 
+      </p>
+      
+      <button onclick="removeBook(${bookData[i].id})">Remove</button>
+    </div>`;
+  }
+}
+
+window.addEventListener('load', () => {
+  showBooks();
+});
